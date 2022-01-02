27b9a7d7d052bb3f185a38d37bc35444ee618843
pinar <pinarunalcan50@gmail.com>
2022-01-02 23:32:25 +0100

    US06_pinar

---
+++ src/test/java/pages/US06_DisappearingElementsPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class US06_DisappearingElementsPage {
+    
+    public US06_DisappearingElementsPage() {
+        PageFactory.initElements(utilities.Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//a[.='Disappearing Elements']")
+    public WebElement disappearingElementsLink;
+
+
+    @FindBy(xpath = "//ul/li/a")
+    public List<WebElement> disappearingElementsMenuList;
+
+   
+    @FindBy(xpath = "(//ul/li)[1]")
+    public WebElement home;
+
+
+    @FindBy(xpath = "//h1")
+    public WebElement homePageTitle;
+
+
+    @FindBy(xpath = "//h1[.='Not Found']")
+    public WebElement notFoundMessage;
+}
---
+++ src/test/java/tests/US06_DisappearingElementsTest.java
@@ -0,0 +1,87 @@
+package tests;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pages.US06_DisappearingElementsPage;
+import pages.HomePage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+public class US06_DisappearingElementsTest {
+    
+
+
+    HomePage homePage = new HomePage();
+   US06_DisappearingElementsPage  disappearingElements = new US06_DisappearingElementsPage();
+   Actions actions=new Actions(Driver.getDriver());
+
+
+   @BeforeTest
+   public void setup() {
+
+       Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+       ReusableMethods.waitForPageToLoad(10);
+       ReusableMethods.waitFor(2);
+       disappearingElements.disappearingElementsLink.click();
+   }
+    //* Disappearing Elements sayfasinda 5 tane menu basligi oldugunu dogrulayiniz
+   @Test
+   public void TC0601(){
+       int menuListSize=disappearingElements.disappearingElementsMenuList.size();
+       System.out.println(menuListSize);
+       Assert.assertEquals(menuListSize, 5);
+
+
+    
+}
+//*Home menusune tiklandiginda anasayfaya gidildigini dogrulayiniz
+@Test
+public void TC0602(){
+    disappearingElements.disappearingElementsMenuList.get(0).click();
+    System.out.println( disappearingElements.homePageTitle.getText());
+    disappearingElements.homePageTitle.isDisplayed();
+}
+
+//*About, Contact Us,Portfolio menulere tiklandiginda "Not Found" mesajinin goruntulendigini dogrulayiniz
+@Test
+public void TC0603(){
+  
+    for (int i = 1; i < disappearingElements.disappearingElementsMenuList.size(); i++) {
+        disappearingElements.disappearingElementsMenuList.get(i).click();
+        Assert.assertEquals(disappearingElements.notFoundMessage.getText(),"Not Found");
+       // Driver.getDriver().navigate();
+       System.out.println(disappearingElements.notFoundMessage.getText());
+       Driver.getDriver().get("http://the-internet.herokuapp.com/disappearing_elements");
+    }
+  
+  
+}
+
+//*Her bir menunun once kirmizi(#DA4B4B) renkte gorundugunu, mouse ile ustune gidildiginde
+//* siyah(#000) renge donustugunu dogrulayiniz
+@Test
+public void TC0604(){
+   // ReusableMethods.waitForVisibility(disappearingElements.disappearingElementsMenuList.get(0), 10);
+  for (WebElement w : disappearingElements.disappearingElementsMenuList) {
+      ReusableMethods.waitFor(2);
+     String colorBefore=w.getCssValue("color"); //rgba(34, 34, 34, 1)
+     String hexBefore=Color.fromString(colorBefore).asHex();
+     System.out.println(hexBefore);
+    Assert.assertEquals(hexBefore, "#da4b4b");
+
+    actions.moveToElement(w).perform();
+    
+     String colorAfter=w.getCssValue("color");
+     String hexAfter=Color.fromString(colorAfter).asHex();
+     Assert.assertEquals(hexAfter, "#000000");
+     System.out.println(hexAfter);
+  }
+   
+}
+
+}
