260c67a95e6866acfd0e23522331a2b7c15ecdb4
selmanland <selmanlanka@gmail.com>
2022-02-01 23:31:41 +0100

    login

---
+++ cypress/integration/pages/LoginPage_WithCredential.js
@@ -0,0 +1,94 @@
+//////<reference types="cypress"/>
+
+const username = "tomsmith"
+const password = "SuperSecretPassword!"
+
+
+class LoginPage_WithCredential {
+
+
+    visit() {
+        cy.visit("https://the-internet.herokuapp.com/login")
+        return this
+    }
+
+    visitAssertion() {
+        cy.xpath("//h2[text()='Login Page']").should('have.text', 'Login Page')
+
+
+        cy.xpath("//h2[text()='Login Page']").invoke('text').then((pageText) => {
+            cy.log(pageText)
+
+
+
+        })
+
+    }
+
+    login() {
+
+        // cy.get("input[type='text']").type(username)
+        // cy.get("input[type='password']").type(password)
+        // cy.get("button[type='submit']").click()
+
+        cy.fixture("login_credentials").then((user) => {
+
+
+            cy.get("input[name='username']").type(user.username)
+            cy.get("input[name='password']").type(user.userPassword)
+            cy.get("button[type='submit']").click()
+
+
+
+        })
+
+
+
+    }
+    message() {
+        cy.get("#flash").should('include.text', 'You logged into a secure area!')
+        cy.get("#flash").invoke('text').then((textmessage) => {
+            cy.log(textmessage)
+        })
+
+        cy.xpath("//h2[normalize-space()='Secure Area']").should('include.text', 'Secure Area')
+        cy.xpath("//h2[normalize-space()='Secure Area']").invoke('text').then((pageTitle) => {
+            cy.log(pageTitle)
+        })
+
+
+    }
+
+    logout() {
+        cy.get(".icon-2x.icon-signout").click()
+    }
+
+    logoutAssertion() {
+        cy.get("#flash").should("include.text", "You logged out of the secure area!")
+        cy.get("#flash").invoke('text').then((message) => {
+            cy.log(message)
+        })
+    }
+
+    loginInvalidCredentials() {
+        cy.fixture("login_credentials").then((user) => {
+
+            cy.get("input[name='username']").type(user.fakeUser)
+            cy.get("input[name='password']").type(user.fakePassword)
+            cy.get("button[type='submit']").click()
+
+        })
+        cy.get("#flash").should("include.text", " Your username is invalid!")
+
+
+    }
+
+    cssAssertion(){
+        cy.get("radius").trigger('mouseover').should('have.css','color','rgb(133, 133, 133)) ')
+    }
+
+
+
+
+}
+export default LoginPage_WithCredential
